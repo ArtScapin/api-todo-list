36916dd746dc47053f333e7e6ab6610f04be2b46
Arthur Scapin Pereira <arthurscapin19@gmail.com>
2023-10-06 23:09:23 -0300

    Refactor and Update Item

--- src/main/java/br/csi/apitodolist/controller/ItemController.java
+++ src/main/java/br/csi/apitodolist/controller/ItemController.java
@@ -2,55 +2,57 @@ package br.csi.apitodolist.controller;
 
 import br.csi.apitodolist.model.item.Item;
 import br.csi.apitodolist.model.list.List;
-import br.csi.apitodolist.model.workspace.Workspace;
 import br.csi.apitodolist.service.ItemService;
 import br.csi.apitodolist.service.ListService;
-import br.csi.apitodolist.service.WorkspaceService;
 import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
     private final ItemService service;
     private final ListService listService;
-    private final WorkspaceService workspaceService;
 
-    public ItemController(ItemService service, ListService listService, WorkspaceService workspaceService) {
+    public ItemController(ItemService service, ListService listService) {
         this.service = service;
         this.listService = listService;
-        this.workspaceService = workspaceService;
     }
 
     @PostMapping("/{id}")
     @Transactional
-    public ResponseEntity<Item> store(@PathVariable Long id, @RequestBody Item item, UriComponentsBuilder uriComponentsBuilder) {
+    public ResponseEntity<Item> store(@PathVariable Long id, @RequestBody Item item) {
         List list = listService.findList(id);
-        Workspace workspace = workspaceService.findWorkspace(list.getWorkspace().getId());
-        if (workspace != null) {
+        if (list != null) {
             item.setList(list);
-            item.setStatus(false);
+            item.setStatus(true);
             this.service.create(item);
-            URI uri = uriComponentsBuilder.path("/item/{id}").buildAndExpand(item.getId()).toUri();
-            return ResponseEntity.created(uri).body(item);
+            return ResponseEntity.status(200).body(item);
         }
         return ResponseEntity.notFound().build();
     }
 
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Item> update(@PathVariable Long id, @RequestBody Item item) {
+        return ResponseEntity.status(200).body(this.service.update(item, id));
+    }
+
+    @PutMapping("/change-status/{id}")
+    @Transactional
+    public ResponseEntity<Item> changeStatus(@PathVariable Long id) {
+        return ResponseEntity.status(200).body(this.service.changeStatus(id));
+    }
+
     @GetMapping("/all/{id}")
-    public java.util.List<Item> findItemsBylist(@PathVariable Long id) {
+    public ResponseEntity<java.util.List<Item>> findItemsByList(@PathVariable Long id) {
         List list = listService.findList(id);
-        Workspace workspace = workspaceService.findWorkspace(list.getWorkspace().getId());
-        return this.service.findItemsByList(list.getId());
+        return ResponseEntity.status(200).body(this.service.findItemsByList(list.getId()));
     }
 
     @GetMapping("/{id}")
-    public Item findById(@PathVariable Long id){
-        return this.service.findItem(id);
+    public ResponseEntity<Item> findById(@PathVariable Long id){
+        return ResponseEntity.status(200).body(this.service.findItem(id));
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/br/csi/apitodolist/service/ItemService.java
+++ src/main/java/br/csi/apitodolist/service/ItemService.java
@@ -2,9 +2,8 @@ package br.csi.apitodolist.service;
 
 import br.csi.apitodolist.model.item.Item;
 import br.csi.apitodolist.model.item.ItemRepository;
+import br.csi.apitodolist.model.list.ListRepository;
 import br.csi.apitodolist.model.user.User;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,15 +11,54 @@ import java.util.List;
 @Service
 public class ItemService {
     private final ItemRepository repository;
+    private final ListRepository listRepository;
     private final UserService userService;
 
-    public ItemService (ItemRepository repository, UserService userService) {
+    public ItemService (ItemRepository repository, UserService userService, ListRepository listRepository) {
         this.repository = repository;
         this.userService = userService;
+        this.listRepository = listRepository;
     }
 
     public void create(Item item) {
         this.repository.save(item);
+        br.csi.apitodolist.model.list.List list = item.getList();
+        list.setStatus(false);
+        this.listRepository.save(list);
+    }
+
+
+    public Item update(Item itemData, Long id) {
+        User user = this.userService.getAuthenticatedUser();
+        Item item = this.repository.findByIdAndListWorkspaceUserId(id, user.getId());
+        item.setName(itemData.getName());
+        this.repository.save(item);
+        return item;
+    }
+
+    public Item changeStatus(Long id) {
+        User user = this.userService.getAuthenticatedUser();
+        Item item = this.repository.findByIdAndListWorkspaceUserId(id, user.getId());
+        br.csi.apitodolist.model.list.List list = item.getList();
+        if(item.isStatus()) {
+            item.setStatus(false);
+            this.repository.save(item);
+
+            if(list.isStatus()) {
+                list.setStatus(false);
+                this.listRepository.save(list);
+            }
+        } else {
+            item.setStatus(true);
+            this.repository.save(item);
+
+            List<Item> items = this.findItemsByList(list.getId());
+            if(!items.stream().anyMatch(element -> !element.isStatus())){
+                list.setStatus(true);
+                this.listRepository.save(list);
+            }
+        }
+        return item;
     }
 
     public List<Item> findItemsByList(Long id) {
@@ -35,6 +73,12 @@ public class ItemService {
     public void deleteItem(Long id) {
         User user = this.userService.getAuthenticatedUser();
         Item item = this.repository.findByIdAndListWorkspaceUserId(id, user.getId());
+        br.csi.apitodolist.model.list.List list = item.getList();
         this.repository.delete(item);
+        List<Item> items = this.findItemsByList(list.getId());
+        if(!items.stream().anyMatch(element -> !element.isStatus())){
+            list.setStatus(true);
+            this.listRepository.save(list);
+        }
     }
 }
